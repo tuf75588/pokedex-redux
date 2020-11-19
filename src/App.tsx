702a48e9fd18e873d09d04cb8a98e68005899a6f
Andrew Davis <atd285@gmail.com>
2020-11-19 17:20:43 -0500

    configure tailwind a bit more proper

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 
 function App() {
-  return <div>pokemon pokedex</div>;
+  return (
+    <div className="bg-white">
+      <h1 className="text-gray-900 dark:text-white">Dark mode is here!</h1>
+      <p className="text-gray-600 dark:text-gray-300">Lorem ipsum...</p>
+    </div>
+  );
 }
 
 export default App;
