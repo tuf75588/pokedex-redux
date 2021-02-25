7318b6ea9c0ff8749e811296df74487bb05ee440
Andrew Davis <atd285@gmail.com>
2021-02-25 16:31:22 -0500

    homepage rendered, still some css bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { HelmetProvider, Helmet } from 'react-helmet-async';
-import { Provider } from 'react-redux';
-import Layout from './components/Layout';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
+import SplashScreen from './components/SplashScreen';
+import {
+  getCachedPokemons,
+  cachedPokemonsSelector,
+} from './features/cachedPokemonSlice';
+import { SliceStatus } from './globals';
+
 function App() {
+  const dispatch = useDispatch();
+  const cachedPokemons = useSelector(cachedPokemonsSelector);
+  useEffect(() => {
+    dispatch(getCachedPokemons());
+    // eslint-disable-next-line
+  }, []);
   return (
-    <div className="bg-white">
-      <Layout title="home">future homepage components will go here</Layout>
-    </div>
+    <React.Fragment>
+      {cachedPokemons.status.state === SliceStatus.LOADING ||
+      cachedPokemons.status.state === SliceStatus.IDLE ? (
+        <SplashScreen />
+      ) : (
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      )}
+    </React.Fragment>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { Provider } from 'react-redux';
 import './index.css';
 import './tailwind.output.css';
+import store from './features/store';
 
-render(<App />, document.getElementById('root'));
+render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+      <App />
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
