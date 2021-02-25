d21e0e5d0c922614cdb25d87c96a29af110a4b36
Andrew Davis <atd285@gmail.com>
2021-02-25 15:14:27 -0500

    finish pokemons page view

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import Layout from './components/Layout';
-
 function App() {
   return (
     <div className="bg-white">
