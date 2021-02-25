091c17822551c7e349cf7a3956c685933fa694c5
Andrew Davis <atd285@gmail.com>
2021-02-25 15:36:33 -0500

    configure app routing

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,3 +1,49 @@
 import React from 'react';
-import {Switch, Route, useLocation} from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 import SplashScreen from './components/SplashScreen';
+import PokemonDetailsPage from './pages/PokemonDetailsPage';
+
+// lazy load our homepage
+const Homepage = React.lazy(() => import('./pages/PokemonsPage'));
+
+function Routes(): JSX.Element {
+  const location = useLocation();
+  const transitions = useTransition(location, (location) => location.pathname, {
+    config: {
+      duration: 250,
+    },
+    from: {
+      opacity: 0.25,
+    },
+    enter: {
+      opacity: 1,
+    },
+    leave: {
+      opacity: 0.25,
+    },
+  });
+  return (
+    <React.Suspense fallback={<SplashScreen />}>
+      {transitions.map(({ item: location, props, key }) => {
+        return (
+          <animated.div
+            key={key}
+            style={{
+              ...props,
+              width: '100%',
+              position: 'absolute',
+            }}
+          >
+            <Switch location={location}>
+              <Route exact path="/" component={Homepage} />
+              <Route path="/pokemons/:id" component={PokemonDetailsPage} />
+            </Switch>
+          </animated.div>
+        );
+      })}
+    </React.Suspense>
+  );
+};
+
+export default Routes;
